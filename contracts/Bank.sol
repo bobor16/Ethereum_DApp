64a30ed232fde7f61bcc36811182b56e32327b2a
Borgar Bordoy <borgarbordoy530@gmail.com>
2022-11-01 10:58:05 +0100

    added tdd and contract functions

---
+++ contracts/Bank.sol
@@ -0,0 +1,12 @@
+/**
+Defines the version of the compiler 
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0;
+
+contract Bank {
+    function deposit(uint256 amount) public payable {}
+
+    function balanceOf(address account) public view returns (uint256) {}
+}
