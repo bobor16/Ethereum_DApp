9b1e1f1357b6c93d2f8beb40fbdda244f0f8bf39
Borgar Bordoy <borgarbordoy530@gmail.com>
2022-11-01 11:09:38 +0100

    added test for withdraw

--- test/Bank.test.js
+++ test/Bank.test.js
@@ -17,4 +17,29 @@ contract("Bank", async (accounts) => {
 
         assert.equal(balance, 10)
     })
+
+    it("allows a user to withdraw funds", async () => {
+        const bank = await Bank.new()
+        const depositor = accounts[2]
+
+        const amount = web3.utils.toWei('20', 'ether')
+
+        await bank.deposit({
+            from: depositor,
+            value: amount
+        })
+
+        let balance = await bank.balanceOf(depositor)
+        balance = parseInt(web3.utils.fromWei(balance, 'ether'))
+        assert.equal(balance, 20)
+
+        const withdraw_amount = web3.utils.toWei('10', 'ether')
+        await bank.withdraw(withdraw_amount, {
+            from: depositor
+        })
+
+        balance = await bank.balanceOf(depositor)
+        balance = parseInt(web3.utils.fromWei(balance, 'ether'))
+        assert.equal(balancem, 10)
+    })
 })
